8ece80d8b166efd35fc3e6f0984569b3ef18fec6
Barbara Acosta <u910499@proveedor.teco.com.ar>
2021-06-16 16:05:00 -0300

    first commit

---
+++ src/components/AddCategory.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types';
+
+export const AddCategory = ({setCategories}) => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputchange = (e) => {
+        setInputValue(e.target.value);
+    }
+    const handlesubmit = (e) => {
+        e.preventDefault();
+        if (inputValue.trim().length>2) {
+            setCategories(cats=>[inputValue, ...cats]);
+            setInputValue('');
+        }
+        
+    }
+
+    return (
+        <form onSubmit={handlesubmit}>
+            <input
+                type="text"
+                value={inputValue}
+                onChange={handleInputchange}
+            />
+        </form>
+    )
+}
+
+AddCategory.propTypes={
+    setCategories:PropTypes.func.isRequired
+}
